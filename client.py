2d8a241116743fb09d935cbfe3caefafd2a1d4ed
Anubissibuna <149871262+Anubissibuna@users.noreply.github.com>
2023-11-08 04:44:05 +1000

    Конечная Работа

--- client.py
+++ client.py
@@ -1,12 +1,36 @@
 import socket
 import tkinter as tk
+from socket import *
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(("127.0.0.1",12345))
+class DarkSoulsApp:
+    # Инициализация приложения клиента + подключение к серверу
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.title('Die?')
+        self.win.geometry("256x256+400+300")
+        self.win.config(bg='#D8BFD8')
 
-while True:
-    data = client.recv(2048)
-    print(data.decode("utf-8"))
+        self.client = socket(AF_INET, SOCK_STREAM)
+        self.client.connect(("127.0.0.1", 12345))
 
-    client.send(input().encode("utf-8"))
+        self.setup()
+
+    # Кнопка в приложении
+    def setup(self):
+        btn = tk.Button(self.win, background='#D8BFD8', width=4, height=2, text='Die?', font="Times 35",
+                        command=self.click)
+        btn.place(relx=0.5, rely=0.5, anchor='center')
+        btn.focus()
+
+    # Кликабельность
+    def click(self):
+        self.client.send(bytes("\00", 'ascii'))
+
+    def run(self):
+        self.win.mainloop()
+
+
+if __name__ == "__main__":
+    app = DarkSoulsApp()
+    app.run()
